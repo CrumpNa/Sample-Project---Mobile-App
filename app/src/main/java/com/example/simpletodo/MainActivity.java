f97541778d9561ed633381da2614ebebf7615d4d
CrumpNa <nlcrump99@gmail.com>
2021-08-30 16:39:31 -0400

    2nd commit

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//
 
         btnAdd= findViewById(R.id.btnAdd);
         etItem= findViewById(R.id.etItem);
